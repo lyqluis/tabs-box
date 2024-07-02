ba19cc8bad3b38af1b5572b7f623a03291b37049
lyqluis <woyaozhucelushi@163.com>
2024-07-02 15:35:12 +0800

    fix(component): update collection with sorted by updated time in SideBar

--- src/tabs/index.d.ts
+++ src/tabs/index.d.ts
@@ -2,8 +2,9 @@ interface Collection {
   id: string
   created: number
   updated: number
-  sourceType: 'local-client' | 'transfer'  // local-client | transfer
+  sourceType: "local-client" | "transfer" // local-client | transfer
   title?: string
   name?: string
+  pinned?: string // pinned time
   windows: chrome.windows.Window[]
 }
