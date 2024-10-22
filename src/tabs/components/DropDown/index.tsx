b8322b4a10183d4dbd3fd2dc068460005784ccc7
lyqluis <woyaozhucelushi@163.com>
2024-10-22 14:53:17 +0800

    refactor: Dropdown component

--- src/tabs/components/DropDown/index.tsx
+++ src/tabs/components/DropDown/index.tsx
@@ -1,13 +1,24 @@
 import {
-  forwardRef,
   useEffect,
-  useImperativeHandle,
   useRef,
+  useState,
   type CSSProperties,
+  type FC,
   type ReactNode
 } from "react"
+import { createPortal } from "react-dom"
 
-type DropDownProps = {
+const initStyle = {
+  left: "-99999px",
+  top: "-99999px",
+  opacity: 0,
+  transform: "scale(0.95)",
+  transitionTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+  transitionDuration: "200ms",
+  transitionProperty: "opacity, transform"
+}
+
+type DropdownProps = {
   children: ReactNode
   listClassName?: string
   button?: ReactNode
@@ -16,40 +27,76 @@ type DropDownProps = {
   buttonStyle?: CSSProperties
 }
 
-const DropDown = forwardRef((props: DropDownProps, ref: any) => {
-  const { listClassName, buttonClassName, buttonStyle, buttonText, children } =
-    props
-  const activeRef = useRef(null)
+const Dropdown: FC<DropdownProps> = ({
+  buttonText,
+  buttonClassName,
+  children
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [menuStyle, setMenuStyle] = useState<any>(initStyle)
+  const buttonRef = useRef(null)
+  const menuRef = useRef(null)
 
-  const listClass =
-    listClassName ??
-    "menu dropdown-content z-[1] max-h-[50vh] flex-col flex-nowrap overflow-y-scroll rounded-box bg-base-100 p-2 shadow"
-  const buttonClass = buttonClassName ?? "btn btn-xs m-1"
+  const handleToggle = () => setIsOpen(!isOpen)
 
-  useImperativeHandle(ref, () => ({
-    close: () => (activeRef.current.open = false)
-  }))
-
-  const handleClose = () => {
-    if (activeRef.current.open) {
-      activeRef.current.open = false
+  const handleClickOutside = (event) => {
+    // 如果点击事件的目标不是 button/menu 单本身，则关闭下拉菜单
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      !buttonRef.current.contains(event.target)
+    ) {
+      setIsOpen(false)
     }
   }
+
+  // 点击外部关闭下拉菜单
   useEffect(() => {
-    document.body.addEventListener("click", handleClose)
+    document.body.addEventListener("mousedown", handleClickOutside)
     return () => {
-      document.body.removeEventListener("click", handleClose)
+      document.body.removeEventListener("mousedown", handleClickOutside)
     }
   }, [])
 
-  return (
-    <details className="dropdown" ref={activeRef}>
-      <summary style={buttonStyle} className={buttonClass}>
-        {buttonText}
-      </summary>
-      <ul className={listClass}>{children}</ul>
-    </details>
-  )
-})
+  useEffect(() => {
+    if (isOpen && buttonRef.current) {
+      const buttonRect = buttonRef.current.getBoundingClientRect()
+      setMenuStyle({
+        ...menuStyle,
+        top: `${buttonRect.bottom + 5}px`,
+        left: `${buttonRect.left}px`,
+        transform: "scale(1)",
+        opacity: 1
+      })
+    } else if (!isOpen) {
+      setMenuStyle(initStyle)
+    }
+  }, [isOpen])
 
-export default DropDown
+  const DropDownMenu = createPortal(
+    <div
+      style={menuStyle}
+      ref={menuRef}
+      className="menu dropdown-content absolute z-[1] max-h-[50vh] flex-col flex-nowrap overflow-y-scroll rounded-box bg-base-100 p-2 shadow"
+      onClick={handleToggle}
+    >
+      {children}
+    </div>,
+    document.body
+  )
+
+  return (
+    <>
+      <button
+        ref={buttonRef}
+        className={buttonClassName}
+        onClick={handleToggle}
+      >
+        {buttonText ?? "dropdown"}
+      </button>
+      {isOpen && DropDownMenu}
+    </>
+  )
+}
+
+export default Dropdown
