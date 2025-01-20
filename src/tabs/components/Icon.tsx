bdee9c24f7ee4b70717a6b8d30aadc9027d3299a
lyqluis <woyaozhucelushi@163.com>
2025-01-20 17:34:40 +0800

    feat: add built-in styles to Icon component

--- src/tabs/components/Icon.tsx
+++ src/tabs/components/Icon.tsx
@@ -3,15 +3,24 @@ interface IconProps {
   width?: number | string
   height?: number | string
   fill?: string
+  type?: "default" | "warn" | "disabled" | "success"
   Svg: React.FC<React.SVGProps<SVGSVGElement>> // 将 SVG 组件作为 props 传入
 }
 
-const Icon: React.FC<IconProps> = ({
-  Svg,
-  className = "h-5 w-5 fill-slate-700",
-  ...props
-}) => {
-  return <Svg className={className} {...props}></Svg>
+const defaultSize = "h-5 w-5"
+const iconStyles = {
+  default: "fill-slate-700",
+  warn: "fill-red-500",
+  disabled: "fill-slate-300",
+  success: "fill-green-500"
+}
+
+// ? is it necessary to set a default <i> to wrapper <svg>
+const Icon: React.FC<IconProps> = ({ Svg, className, type, ...props }) => {
+  const iconStyle = defaultSize + " " + iconStyles[type ?? "default"]
+  const iconClassName = className ? iconStyle + " " + className : iconStyle
+
+  return <Svg className={iconClassName} {...props}></Svg>
 }
 
 export default Icon
